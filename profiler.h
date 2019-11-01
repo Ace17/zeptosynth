9cb661c244144df5717075d5c85017e0712db009
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-11-01 18:12:23 +0100

    Fix profiler units

--- profiler.h
+++ profiler.h
@@ -13,13 +13,22 @@ struct ProfileScope
   {
     timespec t2;
     clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
-    static double maxSecs = 0;
-    auto deltaSecs = timespec_diff(t1, t2);
+    static double maxMs = 0;
+    auto durationMs = timespec_diff(t1, t2);
 
-    if(deltaSecs > maxSecs)
+    if(durationMs > maxMs)
     {
-      maxSecs = deltaSecs;
-      fprintf(stderr, "Max duration: %.1f ms\n", maxSecs * 1000);
+      maxMs = durationMs;
+      fprintf(stderr, "Max duration: %.3f ms\n", maxMs);
+    }
+
+    if(0)
+    {
+      fprintf(stderr, "% 4.0f |", durationMs);
+
+      for(int i=0; i < int(durationMs);++i)
+        fprintf(stderr, "*");
+      fprintf(stderr, "\n");
     }
   }
 
