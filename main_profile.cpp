b77ad2dc5bb680bba9674872559d8d896bf559b0
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-11-04 09:06:58 +0100

    Add profiling test app

---
+++ main_profile.cpp
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+#include "synth.h"
+
+int main()
+{
+  Synth synth;
+
+  int sampleCount = 1024 * 1024 * 128;
+  const int blockSize = 256;
+
+  auto t1 = clock();
+  while(sampleCount > 0)
+  {
+    float buff[blockSize];
+    synth.run(buff, blockSize);
+    sampleCount -= blockSize;
+  }
+  auto t2 = clock();
+
+  printf("time=%.2f\n", (t2-t1)/double(CLOCKS_PER_SEC));
+  return 0;
+}
+
