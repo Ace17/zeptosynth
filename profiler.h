34f6e0b6925224852345d124410e574e311c4986
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2022-02-05 22:35:55 +0100

    Simplify

--- profiler.h
+++ profiler.h
@@ -2,55 +2,43 @@
 
 #include <time.h> // clock_gettime
 
+inline double get_monotonic_time()
+{
+  struct timespec spec;
+  clock_gettime(CLOCK_MONOTONIC, &spec);
+
+  auto s  = spec.tv_sec;
+  long ms = spec.tv_nsec / 1000000;
+  if (ms > 999) {
+    s++;
+    ms = 0;
+  }
+
+  static long init = s;
+
+  return (s-init) + ms / 1000.0;
+}
+
 struct ProfileScope
 {
   ProfileScope()
+    : t1(get_monotonic_time())
   {
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
   }
 
   ~ProfileScope()
   {
-    timespec t2;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &t2);
-    static double maxMs = 0;
-    auto durationMs = timespec_diff(t1, t2);
+    const double t2 = get_monotonic_time();
+    static double maxDuration = -1;
+    const auto duration = t2 - t1;
 
-    if(durationMs > maxMs)
+    if(duration > maxDuration)
     {
-      maxMs = durationMs;
-      fprintf(stderr, "Max duration: %.3f ms\n", maxMs);
-    }
-
-    if(0)
-    {
-      fprintf(stderr, "% 4.0f |", durationMs);
-
-      for(int i = 0; i < int(durationMs); ++i)
-        fprintf(stderr, "*");
-
-      fprintf(stderr, "\n");
+      maxDuration = duration;
+      fprintf(stderr, "Max duration: %.1f ms\n", maxDuration*1000.0);
     }
   }
 
-  timespec t1;
-
-  static double timespec_diff(timespec start, timespec stop)
-  {
-    timespec result;
-
-    if((stop.tv_nsec - start.tv_nsec) < 0)
-    {
-      result.tv_sec = stop.tv_sec - start.tv_sec - 1;
-      result.tv_nsec = stop.tv_nsec - start.tv_nsec + 1000000000;
-    }
-    else
-    {
-      result.tv_sec = stop.tv_sec - start.tv_sec;
-      result.tv_nsec = stop.tv_nsec - start.tv_nsec;
-    }
-
-    return result.tv_nsec / 1000000.0 + result.tv_sec * 1000.0;
-  }
+  const double t1;
 };
 
