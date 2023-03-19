fa7ab4a892058ba786da671d67d25f39cdbd5020
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2023-03-19 12:07:13 +0100

    gui: begin work on text rendering

--- src/main_gui.cpp
+++ src/main_gui.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #include "SDL.h"
+#include "font.h"
 #include "profiler.h"
 #include "unique.h"
 
@@ -12,31 +13,33 @@ namespace
 
 const char* const vertex_shader = R"(
 attribute vec2 attr_vertex;
+attribute vec2 attr_vertex_uv;
 attribute vec4 attr_color;
-varying vec4 var_color;
+varying vec2 var_UV;
 
 void main(void)
 {
   vec4 pos = vec4(attr_vertex.x, attr_vertex.y, 0, 1);
-  var_color = attr_color;
+  var_UV = attr_vertex_uv;
   gl_Position = pos;
 }
 )";
 
 const char* const fragment_shader = R"(
 precision mediump float;
-
-varying vec4 var_color;
+uniform sampler2D uni_tex;
+varying vec2 var_UV;
 
 void main()
 {
-  gl_FragColor = var_color;
+  gl_FragColor = texture2D(uni_tex, var_UV);
 }
 )";
 
 enum
 {
   attrib_position,
+  attrib_uv,
   attrib_color,
 };
 
@@ -87,10 +90,11 @@ void readInput()
 struct Vertex
 {
   float x, y;
+  float u, v;
   float r, g, b, a;
 };
 
-void drawScreen(GLuint vbo)
+void drawScreen(GLuint vbo, GLuint tex)
 {
   static uint32_t time = 0;
 
@@ -103,24 +107,32 @@ void drawScreen(GLuint vbo)
   const float lfo2 = float(sin(time * 0.031) * 0.6);
 
   const Vertex vertices[] = {
-        {/* xy */ +1, +1, /* rgba */ 1, 0, 0, 1},
-        {/* xy */ +1, -1, /* rgba */ 0, 1, 0, 1},
-        {/* xy */ -1, -1, /* rgba */ 0, 0, 1, 1},
+        {/* xy */ +1, +1, /* uv */ 1, 1, /* rgba */ 1, 0, 0, 1},
+        {/* xy */ -1, -1, /* uv */ 0, 0, /* rgba */ 0, 1, 0, 1},
+        {/* xy */ +1, -1, /* uv */ 1, 0, /* rgba */ 0, 0, 1, 1},
 
-        {/* xy */ -0.5, -0.5f + lfo1, /* rgba */ 1, 1, 1, 1},
-        {/* xy */ -0.5, +0.5, /* rgba */ 1, 1, 1, 1},
-        {/* xy */ +0.5f + lfo2, +0.5, /* rgba */ 1, 1, 1, 1},
+        {/* xy */ +1, +1, /* uv */ 1, 1, /* rgba */ 1, 0, 0, 1},
+        {/* xy */ -1, +1, /* uv */ 0, 1, /* rgba */ 0, 1, 0, 1},
+        {/* xy */ -1, -1, /* uv */ 0, 0, /* rgba */ 0, 0, 1, 1},
+
+        {/* xy */ -0.5, -0.5f + lfo1, /* uv */ 0, 0, /* rgba */ 1, 1, 1, 1},
+        {/* xy */ -0.5, +0.5, /* uv */ 0, 1, /* rgba */ 1, 1, 1, 1},
+        {/* xy */ +0.5f + lfo2, +0.5, /* uv */ 1, 1, /* rgba */ 1, 1, 1, 1},
   };
 
   SAFEGL(glBindBuffer(GL_ARRAY_BUFFER, vbo));
+  SAFEGL(glBindTexture(GL_TEXTURE_2D, tex));
 
   SAFEGL(glEnableVertexAttribArray(attrib_position));
   SAFEGL(glVertexAttribPointer(attrib_position, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(0 * sizeof(float))));
 
-  SAFEGL(glEnableVertexAttribArray(attrib_color));
-  SAFEGL(glVertexAttribPointer(attrib_color, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(2 * sizeof(float))));
+  SAFEGL(glEnableVertexAttribArray(attrib_uv));
+  SAFEGL(glVertexAttribPointer(attrib_uv, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(2 * sizeof(float))));
 
-  SAFEGL(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW));
+  SAFEGL(glEnableVertexAttribArray(attrib_color));
+  SAFEGL(glVertexAttribPointer(attrib_color, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(4 * sizeof(float))));
+
+  SAFEGL(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_DYNAMIC_DRAW));
 
   SAFEGL(glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(*vertices)));
 }
@@ -142,6 +154,44 @@ void showDriverInfo()
   printf("\n");
 
   printf("Using video driver: %s\n", SDL_GetCurrentVideoDriver());
+}
+
+GLuint loadFontTexture()
+{
+  GLuint tex;
+  SAFEGL(glGenTextures(1, &tex));
+
+  struct Color
+  {
+    uint8_t r, g, b, a;
+  };
+  Color pixels[128 * 128]{};
+
+  for(int y = 0; y < 64; ++y)
+  {
+    for(int x = 0; x < 128; ++x)
+    {
+      int col = x / 8;
+      int row = y / 8;
+      int ch = col + 16 * row;
+      if(font8x8_basic[ch][y % 8] & (1 << (x % 8)))
+      {
+        Color c{};
+        c = {255, 255, 255, 255};
+        pixels[x + (128 - 1 - y) * 128] = c;
+      }
+    }
+  }
+
+  glBindTexture(GL_TEXTURE_2D, tex);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 128, 128, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  return tex;
 }
 
 void safeMain()
@@ -179,14 +229,21 @@ void safeMain()
   GLuint vbo;
   SAFEGL(glGenBuffers(1, &vbo));
 
+  GLuint tex = loadFontTexture();
+
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
   while(keepGoing)
   {
     readInput();
-    drawScreen(vbo);
+    drawScreen(vbo, tex);
 
     SDL_GL_SwapWindow(window);
     SDL_Delay(20);
   }
+
+  glDeleteTextures(1, &tex);
 
   SDL_GL_DeleteContext(context);
   SDL_DestroyWindow(window);
